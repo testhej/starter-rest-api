a23440edf5ce892252eba2015eadaba10449c3a0
testhej <120281524+testhej@users.noreply.github.com>
2023-01-04 16:50:16 +0100

    New origin

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const db = require('@cyclic.sh/dynamodb')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(function(req, res) {
-  res.header("Access-Control-Allow-Origin", "https://localhost"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 })
 
